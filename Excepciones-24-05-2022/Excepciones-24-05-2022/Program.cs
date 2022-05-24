51612b1ea658f35d2eeb117947f476f439faa686
SantyMartinez19 <105743090+SantyMartinez19@users.noreply.github.com>
2022-05-24 11:49:20 -0700

    Programa-24/05/2022

---
+++ Excepciones-24-05-2022/Excepciones-24-05-2022/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excepciones_24_05_2022
+{
+    //Alumno: Santy Francisco Martinez Castellanos
+    //N.Control: 21211989
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           int num;
+
+           int cuadrado;
+
+            try
+            {
+
+                Console.Write("Introduce un numero entero:");
+                num = int.Parse(Console.ReadLine());
+                cuadrado = num * num;
+                if (cuadrado > 100000000)
+                {
+                    throw new Exception("El resultado es muy grande");
+                }
+
+                Console.WriteLine("Numero al cuadrado: {0}", cuadrado);
+            }
+
+            catch(OverflowException)
+            {
+                Console.WriteLine("Numero demasiado grande");
+            }
+
+            catch(FormatException )
+            {
+                Console.WriteLine("No cumple con el formato adecuado");
+            }
+
+           
+            catch(Exception e)
+            {
+                Console.WriteLine("Error: {0}", e.Message);
+            }
+
+            
+            
+        }
+    }
+}
